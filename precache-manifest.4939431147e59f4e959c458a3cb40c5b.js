63f134d8aac0f3b0a30c59a17dd1415e9cafc22c
zwelethu <zwelethu@yahoo.com>
2020-02-23 01:51:50 +0200

    Updates

---
+++ precache-manifest.4939431147e59f4e959c458a3cb40c5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4bc4d413a227f4cba4390a8680f961d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "6399e2e4870c9a1f63f2",
+    "url": "/monsters/static/css/main.5760982f.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6399e2e4870c9a1f63f2",
+    "url": "/monsters/static/js/main.b7bae89b.chunk.js"
+  },
+  {
+    "revision": "27edc721a646a18481ad",
+    "url": "/monsters/static/js/runtime-main.9b332845.js"
+  }
+]);
